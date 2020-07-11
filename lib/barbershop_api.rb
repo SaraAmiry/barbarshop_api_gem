3864610faf6c523efc7d58efce9861aefa2b1565
saraamiry <saraamiry24@gmail.com>
2020-07-11 17:18:23 -0400

    initial commit

---
+++ lib/barbershop_api.rb
@@ -0,0 +1,6 @@
+require "barbershop_api/version"
+
+module BarbershopApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/barbershop_api/version.rb
@@ -0,0 +1,3 @@
+module BarbershopApi
+  VERSION = "0.1.0"
+end
